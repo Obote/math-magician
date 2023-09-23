d3e1f0da546fe56ae7f6058c567fd823212e5f2e
Obote <obotedenis256@gmail.com>
2023-09-23 12:40:30 +0300

    Add calculator style

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,26 +1,33 @@
-import "./Calculator.css";
+import './Calculator.css';
 
 function Calculator() {
   return (
     <>
       <div className="calc">
-        <input type="text" placeholder="0" id="answer"></input>
-        <input type="button" value="9"></input>
-        <input type="button" value="8"></input>
-        <input type="button" value="7"></input>
-        <input type="button" value="6"></input>
-        <input type="button" value="5"></input>
-        <input type="button" value="4"></input>
-        <input type="button" value="3"></input>
-        <input type="button" value="2"></input>
-        <input type="button" value="1"></input>
+        <input type="text" placeholder="0" id="answer" />
+        <input type="button" value="AC" className="button" />
+        <input type="button" value="+/-" className="button" />
+        <input type="button" value="%" className="button" />
+        <input type="button" value="÷" className="button orange" />
 
-        <input type="button" value="0"></input>
-        <input type="button" value="+"></input>
-        <input type="button" value="-"></input>
-        <input type="button" value="/"></input>
-        <input type="button" value="*"></input>
-        <input type="button" value="%"></input>
+        <input type="button" value="9" className="button" />
+        <input type="button" value="8" className="button" />
+        <input type="button" value="7" className="button" />
+        <input type="button" value="*" className="button orange" />
+
+        <input type="button" value="6" className="button" />
+        <input type="button" value="5" className="button" />
+        <input type="button" value="4" className="button" />
+        <input type="button" value="-" className="button orange" />
+
+        <input type="button" value="3" className="button" />
+        <input type="button" value="2" className="button" />
+        <input type="button" value="1" className="button" />
+        <input type="button" value="+" className="button orange" />
+
+        <input type="button" value="0" className="button double" />
+        <input type="button" value="." className="button" />
+        <input type="button" value="=" className="button orange" />
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Calculator from "./Calculator";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Calculator from './Calculator';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Calculator />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
